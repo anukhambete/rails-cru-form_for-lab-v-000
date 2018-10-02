413ba3b9e0de20cf590930237c0f6c6f7201b70d
Anu Khambete <anukhambete@gmail.com>
2018-10-02 22:19:04 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,5 @@
 Rails.application.routes.draw do
-  get 'songs/new'
-
-  get 'songs/create'
-
-  get 'songs/edit'
-
-  get 'songs/update'
-
-  get 'songs/index'
-
-  get 'songs/show'
-
-  get 'genres/new'
-
-  get 'genres/create'
-
-  get 'genres/edit'
-
-  get 'genres/update'
-
-  get 'genres/index'
-
-  get 'genres/show'
-
-  get 'artists/new'
-
-  get 'artists/create'
-
-  get 'artists/edit'
-
-  get 'artists/update'
-
-  get 'artists/index'
-
-  get 'artists/show'
+  
 
   resources :songs
   resources :genres
